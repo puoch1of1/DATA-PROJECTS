fed1882c67a8b492c0b563ac07fa1fb3b9da41a8
Puoch Mabor Makuei <136756243+puoch1of1@users.noreply.github.com>
2026-02-01 16:18:47 +0300

    Refactor fetch_indicator method to improve error handling and response logging

--- REST API PROJECT/scripts/worldbank_client.py
+++ REST API PROJECT/scripts/worldbank_client.py
@@ -11,4 +11,18 @@ class WorldBankClient:
         def fetch_indicator(self, country_code, indicator_code):
             url = (
                 f"{self.BASE_URL}/country/{country_code}/indicator/"
+                f"{indicator_code}?format=json&per_page=1000"
             )
+
+            for attempt in range(1, self.retries + 1):
+                try: 
+                    response = requests.get(url, timeout=self.timeout)
+
+                    if response.status_code == 200:
+                        return response.json()
+                    
+                    else: 
+                        print(
+                            f"Attempt {attempt}: "
+                            f"Status {response.status_code}"
+                        )
